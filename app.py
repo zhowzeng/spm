86f1f409f8733d1f85d0c8c0c46019a9cb263190
zhowzeng <zenglihao106@gmail.com>
2023-12-02 18:12:11 +0800

    refactor: add streamlit sample script

---
+++ app.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+DATE_COLUMN = "date/time"
+DATA_URL = (
+    "https://s3-us-west-2.amazonaws.com/"
+    "streamlit-demo-data/uber-raw-data-sep14.csv.gz"
+)
+
+
+# When you mark a function with Streamlit’s cache annotation, it tells Streamlit that whenever the function is called that it should check two things:
+# 1. The input parameters you used for the function call.
+# 2. The code inside the function.
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis="columns", inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+
+st.title("Uber pickups in NYC")
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text("Loading data...")
+# Load 10,000 rows of data into the dataframe.
+data = load_data(10000)
+# Notify the reader that the data was successfully loaded.
+# data_load_state.text("Loading data...done!")
+data_load_state.text("Done! (using st.cache_data)")
+
+if st.checkbox("Show raw data"):
+    st.subheader("Raw data")
+    st.write(data)
+
+st.subheader("Number of pickups by hour")
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0, 24))[0]
+st.bar_chart(hist_values)
+
+hour_to_filter = st.slider("hour", 0, 23, 17)  # min: 0h, max: 23h, default: 17h
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader(f"Map of all pickups at {hour_to_filter}:00")
+st.map(filtered_data)
