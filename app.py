2f7a4f105e663089a3a1304832f368183237e9c7
zhowzeng <zenglihao106@gmail.com>
2023-12-05 22:43:06 +0800

    refactor: basic fake ui

--- app.py
+++ app.py
@@ -2,93 +2,26 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-DATE_COLUMN = "date/time"
-DATA_URL = (
-    "https://s3-us-west-2.amazonaws.com/"
-    "streamlit-demo-data/uber-raw-data-sep14.csv.gz"
+st.title("Stock Pattern Matcher")
+
+
+st.sidebar.title("Configuration")
+ticker_symbol = st.sidebar.selectbox("Ticker Symbol", ("2330.TW", "2890.TW"))
+lookback = st.sidebar.selectbox("Lookback Size", (5, 10, 20, 60), 3)
+horizon = st.sidebar.selectbox("Horizon Size", (5, 10, 20), 2)
+n_neighbors = st.sidebar.slider("Number Of Neighbors", 1, 20, 5, 1)
+
+
+data = pd.DataFrame(
+    np.random.randn(n_neighbors, lookback),
+    columns=(i + 1 for i in range(lookback)),
+    index=(f"stock {i}" for i in range(n_neighbors)),
+).T
+
+
+st.line_chart(
+    data,
+    color=["#ffaa00"] * n_neighbors,
 )
 
-
-# When you mark a function with Streamlit’s cache annotation, it tells Streamlit that whenever the function is called that it should check two things:
-# 1. The input parameters you used for the function call.
-# 2. The code inside the function.
-@st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis="columns", inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
-
-
-dataframe = pd.DataFrame(
-    np.random.randn(10, 20), columns=("col %d" % i for i in range(20))
-)
-
-st.dataframe(dataframe.style.highlight_max(axis=0))
-st.table(dataframe)
-
-df = pd.DataFrame({"first column": [1, 2, 3, 4], "second column": [10, 20, 30, 40]})
-
-option = st.selectbox("Which number do you like best?", df["first column"])
-
-"You selected: ", option
-
-# Add a selectbox to the sidebar:
-add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?", ("Email", "Home phone", "Mobile phone")
-)
-
-# Add a slider to the sidebar:
-add_slider = st.sidebar.slider("Select a range of values", 0.0, 100.0, (25.0, 75.0))
-
-left_column, right_column = st.columns(2)
-# You can use a column just like st.sidebar:
-left_column.button("Press me!")
-
-# Or even better, call Streamlit functions inside a "with" block:
-with right_column:
-    chosen = st.radio(
-        "Sorting hat", ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin")
-    )
-    st.write(f"You are in {chosen} house!")
-
-import time
-
-"Starting a long computation..."
-
-# Add a placeholder
-latest_iteration = st.empty()
-bar = st.progress(0)
-
-for i in range(100):
-    # Update the progress bar with each iteration.
-    latest_iteration.text(f"Iteration {i+1}")
-    bar.progress(i + 1)
-    time.sleep(0.1)
-
-"...and now we're done!"
-
-
-st.title("Uber pickups in NYC")
-
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text("Loading data...")
-# Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
-# Notify the reader that the data was successfully loaded.
-# data_load_state.text("Loading data...done!")
-data_load_state.text("Done! (using st.cache_data)")
-
-if st.checkbox("Show raw data"):
-    st.subheader("Raw data")
-    st.write(data)
-
-st.subheader("Number of pickups by hour")
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0, 24))[0]
-st.bar_chart(hist_values)
-
-hour_to_filter = st.slider("hour", 0, 23, 17)  # min: 0h, max: 23h, default: 17h
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-st.subheader(f"Map of all pickups at {hour_to_filter}:00")
-st.map(filtered_data)
+st.dataframe(data)
